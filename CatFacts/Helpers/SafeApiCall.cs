8e6c74ff941e0519ea0a90465b33a2023cb4156d
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-07-24 20:23:22 +0200

    CF-4 - adding prevention for cat fact api not available case

---
+++ CatFacts/Helpers/SafeApiCall.cs
@@ -0,0 +1,17 @@
+﻿namespace CatFacts.Helpers;
+
+public static class SafeApiCall
+{
+    public static async Task<T?> ExecuteAsync<T>(Func<Task<T?>> apiCall, Action<Exception>? onError = null)
+    {
+        try
+        {
+            return await apiCall();
+        }
+        catch (Exception ex)
+        {
+            onError?.Invoke(ex);
+            return default;
+        }
+    }
+}
